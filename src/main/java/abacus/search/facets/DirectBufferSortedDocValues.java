c100dec31a0572fe458158afe8558f2c083b3d6f
John Wang <john.wang@gmail.com>
2014-01-12 01:16:25 -0800

    moved bytesref array off heap

--- src/main/java/abacus/search/facets/DirectBufferSortedDocValues.java
+++ src/main/java/abacus/search/facets/DirectBufferSortedDocValues.java
@@ -9,25 +9,31 @@ public class DirectBufferSortedDocValues extends SortedDocValues {
 
   private final ByteBuffer ords;
   private final ByteBuffer buffer;  
-  private final BytesRef[] byteRefs;
+  private final ByteBuffer byteRefs;
+  private final int numTerms;
+  
   public DirectBufferSortedDocValues(SortedDocValues inner, int maxDoc) {    
     this.ords = ByteBuffer.allocateDirect(maxDoc * 4);
     for (int i =0;i< maxDoc; ++i) {      
       this.ords.putInt(inner.getOrd(i));
     }
-    byteRefs = new BytesRef[inner.getValueCount()];
+    
+    numTerms = inner.getValueCount();
+    this.byteRefs = ByteBuffer.allocateDirect(numTerms * 8);
+    BytesRef[] byteRefArr = new BytesRef[numTerms];
     
     int numBytes = 0;
-    for (int i=0;i<byteRefs.length;++i) {
+    for (int i = 0; i < numTerms ; ++i) {
       BytesRef tempRef = new BytesRef();
       inner.lookupOrd(i, tempRef);
-      numBytes+=tempRef.length;
-      byteRefs[i] = tempRef;
+      numBytes += tempRef.length;
+      byteRefArr[i] = tempRef;
+      this.byteRefs.putInt(tempRef.offset);
+      this.byteRefs.putInt(tempRef.length);
     }
-    buffer = ByteBuffer.allocateDirect(numBytes);
-    for (int i=0;i<byteRefs.length;++i) {      
-      buffer.put(byteRefs[i].bytes, byteRefs[i].offset, byteRefs[i].length);
-      byteRefs[i].bytes = null;
+    buffer = ByteBuffer.allocateDirect(numBytes);    
+    for (int i = 0;i < numTerms ;++i) {      
+      buffer.put(byteRefArr[i].bytes, byteRefArr[i].offset, byteRefArr[i].length);
     }
   }
   
@@ -38,17 +44,20 @@ public class DirectBufferSortedDocValues extends SortedDocValues {
 
   @Override
   public void lookupOrd(int ord, BytesRef result) {
-    result.bytes = new byte[byteRefs[ord].length];
+    int offset = byteRefs.getInt(8 * ord );
+    int length = byteRefs.getInt(8 * ord + 4);
+    
+    result.bytes = new byte[length];
     result.offset = 0;
-    result.length = byteRefs[ord].length;
+    result.length = length;
     for (int i = 0; i < result.length; ++i) {
-      result.bytes[i] = buffer.get(byteRefs[ord].offset + i);
+      result.bytes[i] = buffer.get(offset + i);
     }
   }
 
   @Override
   public int getValueCount() {
-    return byteRefs.length;
+    return numTerms;
   }
 
 }
--- src/main/java/abacus/search/facets/NativeSortedDocValues.java
+++ src/main/java/abacus/search/facets/NativeSortedDocValues.java
@@ -13,27 +13,36 @@ public class NativeSortedDocValues extends SortedDocValues
 
   private final long ordsPtr;
   private final long bufferPtr;  
-  private final BytesRef[] byteRefs;
+  //private final BytesRef[] byteRefs;
+  private final long bytesRefPtr;
+  private final int numTerms;
   public NativeSortedDocValues(SortedDocValues inner, int maxDoc) {    
     this.ordsPtr = HS.allocArray(maxDoc, 4, false);
     for (int i =0;i< maxDoc; ++i) {     
       HS.setInt(this.ordsPtr, i, inner.getOrd(i));
     }
     
-    byteRefs = new BytesRef[inner.getValueCount()];
+    
+    numTerms = inner.getValueCount();
+    BytesRef[] byteRefs = new BytesRef[numTerms];
+    bytesRefPtr= HS.allocArray(numTerms, 8, false);
     
     int numBytes = 0;
-    for (int i=0;i<byteRefs.length;++i) {
+    for (int i=0; i < numTerms; ++i) {
       BytesRef tempRef = new BytesRef();
       inner.lookupOrd(i, tempRef);
       numBytes+=tempRef.length;
       byteRefs[i] = tempRef;
+      HS.setInt(bytesRefPtr, i*2, tempRef.offset);
+      HS.setInt(bytesRefPtr, i*2 + 1, tempRef.length);
     }
     
     bufferPtr = HS.allocArray(numBytes, 1, false);
-    for (int i=0;i<byteRefs.length;++i) {
-      for (int k = 0; k < byteRefs[i].length; ++k) {
-        HS.unsafe.putByte(bufferPtr + k, byteRefs[i].bytes[byteRefs[i].offset + k]);
+    for (int i=0;i < numTerms; ++i) {
+      int offset = byteRefs[i].offset;
+      int length = byteRefs[i].length;
+      for (int k = 0; k < length; ++k) {
+        HS.unsafe.putByte(bufferPtr + k, byteRefs[i].bytes[offset + k]);
       }      
       byteRefs[i].bytes = null;
     }
@@ -46,25 +55,26 @@ public class NativeSortedDocValues extends SortedDocValues
 
   @Override
   public void lookupOrd(int ord, BytesRef result) {
-    result.bytes = new byte[byteRefs[ord].length];
+    int offset = HS.getInt(bytesRefPtr, 2 * ord);
+    int length = HS.getInt(bytesRefPtr, 2 * ord + 1);
+    
+    result.bytes = new byte[length];
     result.offset = 0;
-    result.length = byteRefs[ord].length;
+    result.length = length;
     for (int i = 0; i < result.length; ++i) {
-      result.bytes[i] = HS.unsafe.getByte(bufferPtr + byteRefs[ord].offset + i);  
+      result.bytes[i] = HS.unsafe.getByte(bufferPtr + offset + i);  
     }    
   }
 
   @Override
   public int getValueCount() {
-    return byteRefs.length;
+    return numTerms;
   }
 
   @Override
   public void close() throws IOException {
     HS.freeArray(bufferPtr);
     HS.freeArray(ordsPtr);
-  }
-  
-  
-
+    HS.freeArray(bytesRefPtr);
+  }  
 }
