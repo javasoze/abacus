e408940d3d694e74089361cf5fa345fff6acd266
John Wang <johnwang@twitter.com>
2014-02-24 15:19:17 -0800

    cleanup

---
+++ bin/clue.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+bin=`dirname "$0"`
+bin=`cd "$bin"; pwd`
+
+lib=$bin/../abacus-cli/target/lib
+dist=$bin/../abacus-cli/target
+
+HEAP_OPTS="-Xmx1g -Xms1g -XX:NewSize=256m"
+JAVA_OPTS="-server -d64"
+
+MAIN_CLASS="com.senseidb.clue.ClueApplication"
+CLASSPATH=$CLASSPATH:$lib/*:$dist/*:$1/ext/*
+
+(cd $bin/..; java $JAVA_OPTS $JMX_OPTS $HEAP_OPTS -classpath $CLASSPATH $MAIN_CLASS $@)
